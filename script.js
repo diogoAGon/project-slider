e1e97f1418ebf743529d467ffaf68dc56f264006
devdiogo1 <dev1diogo@outlook.com>
2023-11-01 09:16:45 -0300

    Adicionando parte dos tenis, e finalizando.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    const  sliderImages = document.querySelectorAll(".slider-img");
+    const  sliderImages = document.querySelectorAll(".slider-img",".slider-img2" );
+  
+    sliderImages.forEach(function (image) {
+      image.addEventListener("click", function () {
+        
+        sliderImages.forEach(function (img) {
+          img.classList.remove("active");
+        });
+        this.classList.add("active");
+        
+      });
+    });
+  });
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const sliderImages = document.querySelectorAll(".slider-img2");
   
     sliderImages.forEach(function (image) {
       image.addEventListener("click", function () {
@@ -12,5 +28,3 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
-
-
